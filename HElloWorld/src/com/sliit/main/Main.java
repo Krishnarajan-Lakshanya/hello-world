b201170b53a42b155c612156f3a4126da78dcb00
Krishnarajan-Lakshanya <it16143050@my.sliit.lk>
2020-04-15 11:49:19 +0530

    first commit

---
+++ HElloWorld/src/com/sliit/main/Main.java
@@ -0,0 +1,12 @@
+package com.sliit.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my first project in eclipse to connect with GIT");
+		System.out.println("try commit");
+
+	}
+
+}
